30e3288859460a2e8e0a7cb2d481d2555e7d257c
Kauã Alexandre Bernardes <kauabernardes001@gmail.com>
2026-03-24 06:14:44 -0300

    feat: listar e entrar em comunidade

--- src/community/community.controller.ts
+++ src/community/community.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { CommunityService } from './community.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { Create } from './dto/create.dto';
+import { PaginationDto } from 'src/util/dto/pagination.dto';
 
 @Controller('community')
 export class CommunityController {
@@ -11,5 +21,17 @@ export class CommunityController {
   @Post()
   async create(@Request() req, @Body() payload: Create) {
     return this.communityService.create(payload, req.user.sub);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post(':id/join')
+  async join(@Param('id') id: string, @Request() req) {
+    return this.communityService.join(id, req.user.sub);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('recommended')
+  async getRecommended(@Request() req, @Query() pagination: PaginationDto) {
+    return this.communityService.getRecommended(req.user.sub, pagination);
   }
 }
--- src/community/community.service.ts
+++ src/community/community.service.ts
@@ -1,6 +1,11 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Create } from './dto/create.dto';
+import { PaginationDto } from 'src/util/dto/pagination.dto';
 
 @Injectable()
 export class CommunityService {
@@ -27,6 +32,72 @@ export class CommunityService {
       return {
         message: 'Comunidade criada com sucesso',
         community: community,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+  async join(communityId: string, userId: string) {
+    const community = await this.prisma.community.findUnique({
+      where: { id: communityId },
+    });
+
+    if (!community) {
+      throw new NotFoundException('Comunidade não encontrada');
+    }
+
+    const memberExist = await this.prisma.member.findUnique({
+      where: {
+        userId_communityId: { userId, communityId },
+      },
+    });
+
+    if (memberExist) {
+      throw new ConflictException('Você já faz parte desta comunidade');
+    }
+
+    const member = await this.prisma.member.create({
+      data: { userId, communityId },
+    });
+
+    return {
+      message: 'Inscrição realizada com sucesso',
+      member,
+    };
+  }
+
+  async getRecommended(userId: string, pagination: PaginationDto) {
+    const { page: pageString, limit: limitString } = pagination;
+    const page = Number(pageString);
+    const limit = Number(limitString);
+
+    const skip = (page - 1) * limit;
+
+    try {
+      const [communities, total] = await this.prisma.$transaction([
+        this.prisma.community.findMany({
+          skip: skip,
+          take: limit,
+          orderBy: { createdAt: 'desc' },
+          include: {
+            author: { select: { username: true } },
+            _count: { select: { members: true } },
+          },
+        }),
+        this.prisma.community.count({
+          where: {
+            members: { none: { userId: userId } },
+          },
+        }),
+      ]);
+
+      return {
+        data: communities,
+        meta: {
+          total,
+          page,
+          lastPage: Math.ceil(total / limit),
+        },
       };
     } catch (error) {
       throw error;
