1efd0b75a499a76eaff75842c55b8a13e2470914
Maria Júlia Leopoldo Barros <20232TIIMI0101@cefetes.br>
2026-06-03 14:37:06 -0300

    Commit da nova branch

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 import { User } from 'src/schema/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+  ],
+
+  controllers: [UserController],
+
   providers: [UserService],
+
   exports: [UserService],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
 import { InjectRepository } from '@nestjs/typeorm';
+
 import { Repository } from 'typeorm';
+
 import { User } from 'src/schema/user.entity';
 
 @Injectable()
@@ -13,7 +19,99 @@ export class UserService {
   async findById(id: string) {
     return this.userRepository.findOne({
       where: { id },
-      select: { id: false, username: true, email: true },
+
+      select: {
+        id: false,
+        username: true,
+        email: true,
+      },
     });
+  }
+
+  async getUserProfile(
+    profileUserId: string,
+    loggedUserId: string,
+  ) {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: profileUserId,
+      },
+
+      relations: [
+        'communities',
+        'communities.community',
+        'communities.community.posts',
+      ],
+    });
+
+    if (!user) {
+      throw new NotFoundException(
+        'Usuário não encontrado',
+      );
+    }
+
+    const loggedUser =
+      await this.userRepository.findOne({
+        where: {
+          id: loggedUserId,
+        },
+
+        relations: [
+          'communities',
+          'communities.community',
+        ],
+      });
+
+    const profileCommunityIds =
+      user.communities.map(
+        (community) =>
+          community.community.id,
+      );
+
+    const loggedCommunityIds =
+      loggedUser.communities.map(
+        (community) =>
+          community.community.id,
+      );
+
+    const commonCommunityIds =
+      profileCommunityIds.filter((id) =>
+        loggedCommunityIds.includes(id),
+      );
+
+    const commonPosts = [];
+
+    for (const member of user.communities) {
+      if (
+        commonCommunityIds.includes(
+          member.community.id,
+        )
+      ) {
+        const posts =
+          member.community.posts || [];
+
+        commonPosts.push(...posts);
+      }
+    }
+
+    return {
+      username: user.username,
+
+      email: user.email,
+
+      createdAt: user.createdAt,
+
+      communitiesCount:
+        user.communities.length,
+
+      communities: user.communities.map(
+        (member) => ({
+          id: member.community.id,
+          name: member.community.name,
+        }),
+      ),
+
+      commonPosts,
+    };
   }
 }
